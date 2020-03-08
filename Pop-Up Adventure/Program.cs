483c58bd757c40c8f5e25c7cd593d0c2016b20c7
freercurse <freerjoseph@yahoo.co.uk>
2020-03-08 00:50:07 +0000

    Add project files.

---
+++ Pop-Up Adventure/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+
+namespace Pop_Up_Adventure
+{
+    class Program
+    {
+        string Name;
+        string sex;
+        string age;
+        static void Main(string[] args)
+        {
+            Console.Write("Loading Gaia Engine");
+            Thinking();
+            Thread.Sleep(3000);
+            Console.Clear();
+            GameBegin();
+        }
+
+        private static void GameBegin()
+        {
+            Console.Write("Login Using Stored Credentials?");
+            Console.WriteLine("");
+
+            String Firstchoice = Console.ReadLine().ToString();
+            if (Firstchoice == "y")
+            {
+                Console.Clear();
+                Console.Write("Authenticating");
+                Thinking();
+                Console.Clear();
+                Console.WriteLine("Welcome AUTHENTICATED USER");
+                Thread.Sleep(2000);
+                Console.Clear();
+                
+            }
+            else
+            { 
+                Console.Write("Thinking!");
+                Thinking();
+                Console.WriteLine(" OK, ByeBye then!");
+                Thread.Sleep(3000);
+                return;
+            }      
+                        
+            Thread.Sleep(1000);            
+            Console.Clear();
+            Introduction();
+        }
+
+        private static void Introduction()
+        {
+            Console.Write("INTRODUCTION required!");
+            ThinkingShort();
+            Console.Clear();
+            Console.Write("Loading GUI Introduction Interface");
+            Thinking();
+            Thread.Sleep(2000);
+            Console.Write("ERROR ERROR!");
+            Thread.Sleep(3000);
+            Console.Clear();
+            Console.Write("Low Power Detected");
+            Thinking();
+            Console.WriteLine("");
+            Console.WriteLine("Begin Diagnostics?");
+            Console.ReadLine();
+
+        }
+
+        static void Thinking()
+        {
+            Thread.Sleep(500);
+            Console.Write(".");
+            Thread.Sleep(500);
+            Console.Write(".");
+            Thread.Sleep(500);
+            Console.Write(".");
+            Thread.Sleep(500);
+            Console.Write(".");
+            Thread.Sleep(1000);
+        }
+        static void ThinkingShort()
+        {
+            Thread.Sleep(500);
+            Console.Write(".");
+            Thread.Sleep(500);
+            Console.Write(".");
+            Thread.Sleep(500);
+            Console.Write(".");
+            Thread.Sleep(1000);
+        }
+    }
+}
